d8062821876e5fb0ea124187a0c31fd311fccc04
Gihyeon Yang <dexterastin@gmail.com>
2018-05-04 20:25:13 +0900

    1누르면 공격 그외엔 3초간 걷기

--- communication.py
+++ communication.py
@@ -4,34 +4,31 @@ import time
 
 def main():
     with serial.Serial('/dev/tty.usbserial-AL03ERTX', 56700, timeout=1) as ser:
-        line = [0, 0, 0, 0, 0, 0]
         while True:
+            line = [0, 0, 0, 0, 0, 0]
+            s = input()
+            b = [b'\xff', b'U', b'\x00', b'\xff', b'\x00', b'\xff'] if s == "1" else [
+                b'\xff', b'U', b'\x02', b'\xfd', b'\x00', b'\xff']
+            for i in range(1):
+                for item in b:
+                    ser.write(item)
+
             found = False
             while not found:
                 tmp = ser.read(1)
                 if (tmp == b'\xff'):
-                    line[0] = str(tmp)
+                    line[0] = tmp
                     for i in range(1, 6):
                         line[i] = ser.read(1)
-                        pass
-                    pass
-                    print("\t".join((str(b) for b in line)))
-                    if line[2] != b'\x00':
+
+                    rString = integerToBytes(line)
+                    print(rString)
+                    if line[2] == b'c':
                         found = True
-                        break
-                    pass
-                pass
-            pass
-            s = input()
-            b = [b'\xff', b'U', b'\x00', b'\xff', b'\x00', b'\xff'] if s == "1" else [
-                b'\xff', b'U', b'\x02', b'\xfe',  b'\x02', b'\xfe']
-            for i in range(1):
-                for item in b:
-                    ser.write(item)
-                    pass
-                pass
-            pass
-        pass
+
+
+def integerToBytes(line):
+    return "\t".join(str(int.from_bytes(b, byteorder='big', signed=True)) for b in line)
 
 
 if __name__ == '__main__':
